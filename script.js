a02e1b093602da4c5d3b0433c5436ae5cac2f817
NicoleHielt <nhieltjes@gmail.com>
2024-01-26 14:29:47 +0100

    new files

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+});
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
